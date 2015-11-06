e7c97fc2126631f85083e97535e9c6d572761074
Bennyboy1695 <benny.boy1695@gmail.com>
2015-11-06 12:23:49 +0000

    Updated mcmod.info and RefStrings

--- src/main/java/com/benny/lib/RefStrings.java
+++ src/main/java/com/benny/lib/RefStrings.java
@@ -4,7 +4,7 @@ public class RefStrings {
 
 	public static final String MODID = "hypovolemiaextras";
 	public static final String NAME = "Hypovolemia Extras";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.3";
 	public static final String CLIENTSIDE = "com.benny.HypovolemiaExtras.ClientProxy";
 	public static final String SERVERSIDE = "com.benny.HypovolemiaExtras.ServerProxy";
 	public static final String GUIFACTORY = "com.benny.HypovolemiaExtras.lib.HypoGuiFactory";
