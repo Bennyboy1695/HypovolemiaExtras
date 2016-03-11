8988c675e9a0ff30981ef258a7c04019541aff7d
Ben <benny.boy1695@gmail.com>
2016-03-11 15:38:06 +0000

    Updated version info

--- src/main/java/io/github/bennyboy1695/lib/RefStrings.java
+++ src/main/java/io/github/bennyboy1695/lib/RefStrings.java
@@ -4,7 +4,7 @@ public class RefStrings {
 
 	public static final String MODID = "hypovolemiaextras";
 	public static final String NAME = "Hypovolemia Extras";
-	public static final String VERSION = "1.0.5";
+	public static final String VERSION = "1.0.6";
 	public static final String CLIENTSIDE = "io.github.bennyboy1695.HypovolemiaExtras.ClientProxy";
 	public static final String SERVERSIDE = "io.github.bennyboy1695.HypovolemiaExtras.ServerProxy";
 	public static final String GUIFACTORY = "io.github.bennyboy1695.HypovolemiaExtras.lib.HypoGuiFactory";
